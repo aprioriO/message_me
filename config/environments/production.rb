90c717019d35d195702fc9138a12e269faa686bd
Olena Hulakova <ohulakova@gmail.com>
2020-04-16 00:03:59 +0100

    Set up for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,10 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://olena-message-me.herokuapp.com/cable'
+
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://olena-message-me.herokuapp.com','http://olena-message-me.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
